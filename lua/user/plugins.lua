dbb5647de49654d076fcd26660319cfc60b76e67
OL <oiva.laitinen2@gmail.com>
2024-03-22 22:37:53 +0200

    Add CompetiTest

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -193,7 +193,7 @@ local plugins = {
 		cmd = 'Telekasten',
 		config = function ()
 			require("telekasten").setup({
-				home = "/mnt/c/Users/gr289336/Documents/Notes/" 
+				home = "/mnt/c/Users/gr289336/Documents/Notes/"
 			})
 		end
 	},
@@ -205,8 +205,20 @@ local plugins = {
 			window = { width = 1 },
 			plugins = { tmux = { enabled = true }}
 		}
-	}
-
+	},
+	{
+		"xeluxee/competitest.nvim",
+		dependencies = "muniftanjim/nui.nvim",
+		cmd = "CompetiTest",
+		keys = "<leader>c",
+		opts = {
+			testcases_use_single_file = true,
+			received_contests_directory = "$(HOME)/projects/$(JUDGE)/$(CONTEST)",
+			received_problems_path = "$(HOME)/projects/$(JUDGE)/$(PROBLEM)/main.$(FEXT)",
+			evaluate_template_modifiers = true,
+			template_file = "~/projects/CSES/template.cpp"
+		},
+	},
 }
 
 local lsp = {
