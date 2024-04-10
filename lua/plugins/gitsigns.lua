36da466756d95513a3a6ad5ba4341f71da6570f8
Oiva Laitinen <oiva.laitinen2@gmail.com>
2024-04-10 22:43:05 +0300

    Add gitsigns

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,34 @@
+local config = function() 
+	require("gitsigns").setup {
+		signs = {
+			add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+			change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+			delete = { hl = "GitSignsDelete", text = "", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+			topdelete = { hl = "GitSignsDelete", text = "X", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+			changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+		},
+		signcolumn = true,
+		numhl = false,
+		linehl = false,
+		word_diff = false,
+		watch_gitdir = {
+			interval = 1000,
+			follow_files = true
+		},
+		attach_to_untracked = true,
+		current_line_blame = true,
+		current_line_blame_opts = {
+			virt_text = true,
+			virt_text_pos = "right_align",
+			delay = 1,
+			ignore_whitespace = false,
+		},
+		trouble = true,
+	}
+end
+
+return {
+	'lewis6991/gitsigns.nvim',
+	event = "VeryLazy",
+	config = config,
+}
