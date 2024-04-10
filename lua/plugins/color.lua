5b51efc88cd8665ebaaf379cdcccb89c2490436d
Oiva Laitinen <oiva.laitinen2@gmail.com>
2024-04-10 22:42:39 +0300

    Update color settings to add better support for transparency

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -4,17 +4,31 @@ return {
 	lazy = false,
 	priority = 1000,
 	config = function()
+		local transparent = true,
 		require('catppuccin').setup({
 			flavour = "mocha",
-			transparent = true,
+			transparent_background = true,
 			no_italic = false,
+			dim_inactive = {
+				enabled = true,
+				shade = "dark",
+				percentage = 0.15,
+			},
 			integrations = {
 				treesitter = true,
+				telescope = true,
 				noice = true,
 				notify = true,
 				neotree = true,
 			}
 		})
 		vim.cmd.colorscheme("catppuccin")
+		if transparent and not vim.g.neovide then
+			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+			vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+			vim.api.nvim_set_hl(0, "NormalFloat", {bg="#1E1E2E"})
+			vim.api.nvim_set_hl(0, "LazyNormal", {bg="#1E1E2E"})
+			vim.api.nvim_set_hl(0, "MasonNormal", {bg="#1E1E2E"})
+		end
 	end
 }
