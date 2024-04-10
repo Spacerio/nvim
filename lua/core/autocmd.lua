7252556fd5a978724f5e8fced1592defcbb30979
Oiva Laitinen <oiva.laitinen2@gmail.com>
2024-04-10 20:14:01 +0300

    Move user options to core

---
+++ lua/core/autocmd.lua
@@ -0,0 +1,33 @@
+local os = require("os")
+
+local vim_enter_group = vim.api.nvim_create_augroup("vim_enter_group", { clear = true })
+
+if not vim.fn.has('linux') then
+	vim.api.nvim_create_autocmd(
+		{"VimEnter"},
+
+		{ pattern = "*", command = "cd " .. os.getenv("USERPROFILE"), group = vim_enter_group }
+	)
+end
+
+-- vim.api.nvim_create_autocmd(
+-- 	{"ExitPre"},
+-- 	{
+-- 		callback = function()
+-- 			vim.cmd("LspStop 1 2 3 4 5 6")
+-- 			-- vim.lsp.stop_client(vim.lsp.get_clients())
+-- 		end
+-- 	}
+-- )
+
+-- local function get_git_root()
+-- 	local dot_git_path = vim.fn.finddir(".git", ".;")
+-- 	return vim.fn.fnamemodify(dot_git_path, ":h")
+-- end
+
+-- vim.api.nvim_create_autocmd(
+-- 	{"BufEnter", "BufWinEnter"},
+-- 	{ callback = function ()
+-- 		vim.api.nvim_set_current_dir(get_git_root())
+-- 	end}
+-- )
