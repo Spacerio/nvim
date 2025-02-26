7abe34b9aed9d48c77238ed00bdf278c47f66e26
Oiva Laitinen <oiva.laitinen2@gmail.com>
2025-02-26 14:41:50 +0200

    Fix autopair indentation

--- lua/plugins/autopair.lua
+++ lua/plugins/autopair.lua
@@ -1,8 +1,17 @@
+-- messes with indentation
+-- return {
+--     'altermo/ultimate-autopair.nvim',
+--     event={'InsertEnter','CmdlineEnter' },
+--     branch='v0.6', --recommended as each new version will have breaking changes
+--     opts = {
+-- 		enabled = true,
+--     },
+-- }
+
 return {
-    'altermo/ultimate-autopair.nvim',
-    event={'InsertEnter','CmdlineEnter' },
-    branch='v0.6', --recommended as each new version will have breaking changes
-    opts = {
-		enabled = true,
-    },
+	'windwp/nvim-autopairs',
+	event = "InsertEnter",
+	config = true
+	-- use opts = {} for passing setup options
+	-- this is equivalent to setup{} function
 }
