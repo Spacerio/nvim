e1432cdc169916574a0aa717304eb1eeda2c5361
gr289336 <gr289336@gradia.fi>
2025-02-28 17:21:12 +0200

    Add gui options

--- init.lua
+++ init.lua
@@ -2,6 +2,10 @@ require("core.options")
 require("core.remap")
 require("core.autocmd")
 
+if vim.g.neovide then
+	require("core.gui")
+end
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
---
+++ lua/core/gui.lua
@@ -0,0 +1,12 @@
+vim.g.neovide_fullscreen = true
+vim.g.neovide_scale_factor = 0.8
+
+local opts = { noremap = true, silent = true }
+local map = vim.keymap.set
+-- change scale
+map("n", "<C-=>", "<cmd>let g:neovide_scale_factor=g:neovide_scale_factor+0.1<cr>", opts)
+map("n", "<C-->", "<cmd>let g:neovide_scale_factor=g:neovide_scale_factor-0.1<cr>", opts)
+-- copy and paste
+map('v', '<C-C>', '"+y') -- Copy
+map('n', '<C-S-V>', '"+p') -- Paste normal mode
+map('i', '<C-S-V>', '<c-r>+') -- Paste insert mode
