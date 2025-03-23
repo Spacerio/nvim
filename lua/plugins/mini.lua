44e2572559f4640f1f2ee50b57bf483dfe13cb16
Oiva Laitinen <oiva.laitinen2@gmail.com>
2025-03-23 15:56:16 +0200

    Add mini.nvim

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,21 @@
+-- TODO: enable lazyloading and make all of this more sensible
+return {
+	{
+		'echasnovski/mini.ai',
+		version = false,
+		lazy = false,
+		config = true,
+	},
+	{
+		'echasnovski/mini.surround',
+		version = false,
+		lazy = false,
+		config = true
+	},
+	{
+		'echasnovski/mini.splitjoin',
+		version = false,
+		lazy = false,
+		config = true
+	},
+}
