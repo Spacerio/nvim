4c4a6d63f7e23eec703e39303e999b309ac2936b
Oiva Laitinen <oiva.laitinen2@gmail.com>
2025-03-25 11:36:51 +0200

    Add todo

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,6 +1,6 @@
 -- TODO: enable lazyloading and make all of this more sensible
 return {
-	{
+	{ -- TODO: replace this with pure treesitter textobjects
 		'echasnovski/mini.ai',
 		version = false,
 		lazy = false,
