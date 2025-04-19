11c840be4870ca09d8b30da69ff52ee4560ecfe5
gr289336 <gr289336@gradia.fi>
2025-04-19 19:12:32 +0300

    Move neovide automatic startup directory to autocmd.lua

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -9,6 +9,16 @@ vim.filetype.add({
 	}
 })
 
+if vim.g.neovide then
+	local os = require("os")
+	local path_to_desktop = os.getenv("USERPROFILE") .. "\\Desktop"
+	local vim_enter_group = vim.api.nvim_create_augroup("vim_enter_group", { clear = true })
+	vim.api.nvim_create_autocmd(
+		{"VimEnter"},
+		{ pattern = "*", command = "cd " .. path_to_desktop, group = vim_enter_group }
+	)
+end
+
 -- local vim_enter_group = vim.api.nvim_create_augroup("vim_enter_group", { clear = true })
 --
 -- if not vim.fn.has('linux') then
