ff914a163dd31f84fd67bf42f5b77f0b83f0df45
gr289336 <gr289336@gradia.fi>
2025-04-19 17:19:59 +0300

    Add org-bullets

--- lua/plugins/org.lua
+++ lua/plugins/org.lua
@@ -2,11 +2,18 @@ return {
 	'nvim-orgmode/orgmode',
 	event = 'VeryLazy',
 	ft = { 'org' },
+	dependencies = {
+		'nvim-orgmode/org-bullets.nvim',
+	},
 	config = function()
 		-- Setup orgmode
 		require('orgmode').setup({
 			org_agenda_files = { '~/org/**/*' },
 			org_default_notes_file = '~/org/refile.org',
+			org_hide_leading_stars = true,
+			org_hide_emphasis_markers = true,
+			org_startup_indented = true,
 		})
+		require('org-bullets').setup({})
 	 end,
  }
