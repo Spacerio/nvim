ddb3369bba716bdeddd8152e0dd0690b5a1d7fc8
Oiva Laitinen <oiva.laitinen2@gmail.com>
2025-05-20 22:37:52 +0300

    Update flash config

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,3 +1,20 @@
+local search_mode_opts = {
+	search = {
+		mode = "search",
+		incremental = true,
+	},
+	jump = {
+		nohlsearch = true
+	},
+	highlight = {
+		backdrop = false,
+		matches = true,
+		groups = {
+			current = "IncSearch",
+		}
+	}
+}
+
 return {
 	"folke/flash.nvim",
 	event = "VeryLazy",
@@ -5,7 +22,7 @@ return {
 		autohide = false,
 		modes = {
 			search = {
-				enabled = false,
+				enabled = true,
 			},
 			char = {
 				enabled = false,
@@ -13,9 +30,9 @@ return {
 		}
 	},
 	keys = {
-		{ "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-		{ "\\", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-		-- { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+		{ "s",  mode = { "n", "x", "o" }, function() require("flash").jump() end,                 desc = "Flash" },
+		{ "\\", mode = { "n", "x", "o" }, function() require("flash").jump(search_mode_opts) end, desc = "Flash" },
+		{ "S",  mode = { "n", "x", "o" }, function() require("flash").treesitter_search() end,           desc = "Flash Treesitter" },
 		-- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
 		-- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
 		-- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
